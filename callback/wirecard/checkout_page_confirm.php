55e0f5c4de8bc76cd7e345cf512411abc7e5104e
tomazpu <32056440+tomazpu@users.noreply.github.com>
2017-11-10 17:16:51 +0100

    #9 lang parameter changes and statuses

--- callback/wirecard/checkout_page_confirm.php
+++ callback/wirecard/checkout_page_confirm.php
@@ -285,6 +285,7 @@ debug_msg("-- script reached eof - executed without errors --\n");
 //copy of send_orders.php
 function create_status_mail_for_order($oID, $language)
 {
+    debug_msg("preparing the mail" . $language);
 	$insert_id = (int)$oID;
    
     require_once (DIR_FS_INC.'xtc_get_order_data.inc.php');
